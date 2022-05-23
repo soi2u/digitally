a266aa3a648fc1904834378eebb568453e96c234
soi2u <soi2u@daum.net>
2022-05-23 09:33:05 +0900

    220523
    스노우 js 수정

--- 21/21/js/information.js
+++ 21/21/js/information.js
@@ -6,25 +6,25 @@ for (j = 1; j <= pageNum; j++) {
     audioArray.push(audio);
 }
 
-const page1 = "이번 실습에서는 다나와 앱을 통해 TV 가격을 비교해 보도록 할게요. 먼저, 다나와 앱을 실행해 줍니다.";
-const page2 = "다나와 앱의 홈 화면입니다. TV 검색을 위해 검색창을 눌러 볼게요.";
-const page3 = "검색어를 입력하는 부분에 TV를 적어 볼게요.";
-const page4 = "아래에 보이는 TV 상품을 눌러 이동하겠습니다.";
-const page5 = "다양한 TV 검색 결과가 나옵니다. 카테고리를 보면 제조사, 화면 크기, 기술, 해상도 등 다양한 옵션을 기준으로 확인할 수 있습니다. 제조사 기준으로 되어 있는 카테고리를 화면 크기를 기준으로 변경해 볼게요. 화면 크기를 눌러 줍니다.";
-const page6 = "화면 크기를 기준으로 검색된 결과입니다. 다음은, 화면 기술을 눌러 볼게요.";
-const page7 = "화면 기술을 기준으로 검색된 결과입니다. 마지막으로 해상도를 눌러 볼게요.";
+const page1 = "촬영 앱 스노우를 실습해 보도록 하겠습니다. 앱스토어를 실행해 주세요.";
+const page2 = "앱스토어 검색창에 스노우를 검색해 주세요.";
+const page3 = "스노우를 눌러 설치 화면으로 넘어가 주세요.";
+const page4 = "설치 버튼을 눌러 스노우 앱을 설치해 주세요.";
+const page5 = "설치가 완료되었다면, 열기를 눌러 스노우 앱을 실행해 보겠습니다.";
+const page6 = "이용 약관 동의 화면입니다. 동의하고 시작 옆의 체크 버튼을 눌러 주세요.";
+const page7 = "스노우 앱의 필수 권한 설명입니다. 계속하기 버튼을 눌러 주세요.";
 const page8 =
-    "해상도를 기준으로 TV를 검색한 결과입니다. 이제, 제조사 기준의 결과로 다시 돌아가 볼게요. 제조사를 눌러 줍니다.";
+    "스노우가 미디어에 접근할 수 있도록 허용을 눌러 주세요.";
 const page9 =
-    "제조사 별로 TV 상품을 확인할 수 있습니다. 이번에는 삼성전자의 TV를 확인해 볼게요. 삼성전자를 눌러 줍니다.";
+    "스노우가 촬영 작업을 할 수 있도록 허용을 눌러 주세요,";
 const page10 =
-    "삼성 전자의 tv들이 보입니다. 인기순 버튼을 누르면 다른 기준으로도 정렬할 수 있습니다. 인기순 버튼을 눌러 볼게요.";
-const page11 = "신상품순, 낮은 가격순, 높은 가격순 등 다양한 기준으로 상품을 확인할 수 있습니다. x를 눌러 다시 돌아가 줄게요.";
-const page12 = "이제, 삼성전자의 상품 중 하나를 골라 볼게요. 가장 위에 있는 상품을 골라 줍니다.";
-const page13 = "고른 tv 상품의 별점을 확인할 수 있고, 최저가를 확인할 수 있습니다. 이제 화면을 아래로 내려 볼게요.";
-const page14 = "아래로 내리면, 쇼핑몰 별로 가장 낮은 가격을 확인할 수 있습니다.";
-const page15 = "상품 정보를 누르면, 상품 정보를 확인할 수 있습니다.";
-const page16 = "상품 의견을 누르면, 상품 후기를 확인할 수 있습니다. 모두 고생하셨습니다.";
+    "스노우 앱의 첫 화면입니다. 우측 상단의 회전 버튼을 눌러 볼게요.";
+const page11 = "카메라의 전면, 후면이 변경됩니다. 이제 이펙트를 눌러 볼게요.";
+const page12 = "이펙트를 누르면 사진을 꾸밀 수 있는 다양한 효과가 나옵니다. 뒤로 가기를 눌러 첫 화면으로 돌아갈게요.";
+const page13 = "이번에는 필터를 눌러 보도록 하겠습니다.";
+const page14 = "사진을 다양하게 보정할 수 있는 필터가 나옵니다. 뒤로가기 버튼을 눌러 첫 화면으로 돌아가겠습니다.";
+const page15 = "마지막으로, 하단의 비디오 버튼을 눌러 주세요.";
+const page16 = "사진에서 비디오로 촬영 모드가 전환되었습니다. 기본 카메라뿐만 아니라, 다양한 보정과 이펙트가 있는 스노우를 통해 다양한 사진을 찍을 수 있습니다. 고생하셨습니다. ";
 
 pageDescArray.push(
     page1,
@@ -42,7 +42,7 @@ pageDescArray.push(
     page13,
     page14,
     page15,
-    page16
+    page16,
 );
 
 
@@ -67,73 +67,70 @@ function handleInput(pageNumber) {
 }
 
 const coordinate1 = {
-    startRateX: 0.002777777777777778, startRateY: 0.153125, endRateX: 0.23333333333333334, endRateY: 0.3078125
+    startRateX: 0.011111111111111112, startRateY: 0.54375, endRateX: 0.21388888888888888, endRateY: 0.678125
 };
 
 const coordinate2 = {
-    startRateX: 0.2388888888888889, startRateY: 0.0421875, endRateX: 0.9638888888888889, endRateY: 0.0953125
+    startRateX: 0.06388888888888888, startRateY: 0.0421875, endRateX: 0.6805555555555556, endRateY: 0.0953125
 };
 
 const coordinate3 = {
-    startRateX: 0.09722222222222222, startRateY: 0.0328125, endRateX: 0.4777777777777778, endRateY: 0.1015625
+    startRateX: 0.044444444444444446, startRateY: 0.1171875, endRateX: 0.43333333333333335, endRateY: 0.1765625
 };
 
 const coordinate4 = {
-    startRateX: 0.03611111111111111, startRateY: 0.109375, endRateX: 0.4666666666666667, endRateY: 0.21875
+    startRateX: 0.5694444444444444, startRateY: 0.3984375, endRateX: 0.4222222222222222, endRateY: 0.3546875
 };
 
 const coordinate5 = {
-    startRateX: 0.005555555555555556, startRateY: 0.6421875, endRateX: 0.15833333333333333, endRateY: 0.678125
+    startRateX: 0.6555555555555556, startRateY: 0.2484375, endRateX: 0.7944444444444444, endRateY: 0.3
 };
 
 const coordinate6 = {
-    startRateX: 0.002777777777777778, startRateY: 0.6859375, endRateX: 0.1638888888888889, endRateY: 0.728125
+    startRateX: 0.30833333333333335, startRateY: 0.771875, endRateX: 0.7055555555555556, endRateY: 0.8296875
 };
 
 const coordinate7 = {
-    startRateX: 0.002777777777777778, startRateY: 0.7359375, endRateX: 0.14444444444444443, endRateY: 0.775
+    startRateX: 0.3333333333333333, startRateY: 0.5140625, endRateX: 0.6666666666666666, endRateY: 0.5609375
 };
 
 const coordinate8 = {
-    startRateX: 0.016666666666666666, startRateY: 0.590625, endRateX: 0.16666666666666666, endRateY: 0.6296875
+    startRateX: 0.6777777777777778, startRateY: 0.5171875, endRateX: 0.7916666666666666, endRateY: 0.5703125
 };
 
 const coordinate9 = {
-    startRateX: 0.5027777777777778, startRateY: 0.5484375, endRateX: 0.6472222222222223, endRateY: 0.584375
+    startRateX: 0.6722222222222223, startRateY: 0.4953125, endRateX: 0.7944444444444444, endRateY: 0.55625
 };
 
 const coordinate10 = {
-    startRateX: 0.2722222222222222, startRateY: 0.484375, endRateX: 0.4361111111111111, endRateY: 0.53125
+    startRateX: 0.8861111111111111, startRateY: 0.0140625, endRateX: 0.9805555555555555, endRateY: 0.0640625
 };
 
 const coordinate11 = {
-    startRateX: 0.2722222222222222, startRateY: 0.484375, endRateX: 0.4361111111111111, endRateY: 0.53125
+    startRateX: 0.20555555555555555, startRateY: 0.7640625, endRateX: 0.3527777777777778, endRateY: 0.86875
 };
 
 const coordinate12 = {
-    startRateX: 0.9888888888888889, startRateY: 0.1953125, endRateX: 0.013888888888888888, endRateY: 0.5953125
+    startRateX: 0.15, startRateY: 0.94375, endRateX: 0.29444444444444445, endRateY: 0.9921875
 };
 
 const coordinate13 = {
-    startRateX: 0,
-    startRateY: 0,
-    endRateX: 1,
-    endRateY: 1,
+    startRateX: 0.7972222222222223, startRateY: 0.7734375, endRateX: 0.95, endRateY: 0.8734375
 };
 
 const coordinate14 = {
+    startRateX: 0.013888888888888888, startRateY: 0.8484375, endRateX: 0.14444444444444443, endRateY: 0.9140625
+};
+
+const coordinate15 = {
+    startRateX: 0.5861111111111111, startRateY: 0.8734375, endRateX: 0.7694444444444445, endRateY: 0.9296875
+};
+
+const coordinate16 = {
     startRateX: 0,
     startRateY: 0,
     endRateX: 1,
     endRateY: 1,
-};
-
-const coordinate15 = {
-    startRateX: 0.35833333333333334, startRateY: 0.0421875, endRateX: 0.5305555555555556, endRateY: 0.09375
-};
-
-const coordinate16 = {
-    startRateX: 0.5277777777777778, startRateY: 0.0421875, endRateX: 0.7027777777777777, endRateY: 0.1015625
 };
 
 
