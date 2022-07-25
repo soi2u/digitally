3d655a6cafa84b142933a3ca5805263408ddaa39
soi2u <soi2u@daum.net>
2022-07-25 14:57:36 +0900

    2207254
    image

---
+++ tools/phishing/sms3/css/app.js
@@ -0,0 +1,6 @@
+// 콘텐츠 연동
+var JS = document.createElement("script");
+JS.type = "text/javascript";
+JS.charset = "UTF-8";
+JS.src = "https://ssc.seoul.kr/common/js/content_tracking.js";
+document.getElementsByTagName("head")[0].appendChild(JS);
