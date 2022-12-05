aea275450044c00d76b6cb018cea3a6cb38f9fdb
soi2u <soi2u@daum.net>
2022-12-05 15:54:36 +0900

    221205
    수정

--- 51/51/js/information.js
+++ 51/51/js/information.js
@@ -1,4 +1,4 @@
-const pageNum = 7;
+const pageNum = 8;
 const pageName = "51";
 for (j = 1; j <= pageNum; j++) {
     contentImage.push(`../${pageName}/image/${j}.png`);
@@ -6,13 +6,15 @@ for (j = 1; j <= pageNum; j++) {
     audioArray.push(audio);
 }
 
-const page1 = "우리WON뱅킹의 통장 사본 조회 서비스를 이용해 보도록 하겠습니다. 먼저, 전체 메뉴를 확인하기 위해 우측 하단의 전체를 눌러 줍니다.";
-const page2 = "계좌 조회를 눌러 줍니다.";
-const page3 = "통장사본 조회를 눌러 줍니다.";
-const page4 = "저축예금 부분을 눌러 더 자세하게 알아볼게요.";
-const page5 = "통장사본 저장을 눌러 줍니다.";
-const page6 = "통장사본 저장 페이지입니다. 한 번 눌러 넘어가 줄게요.";
-const page7 = "확인을 누르면 통장 사본이 저장됩니다. 모두 고생하셨습니다.";
+
+const page1 = "우리WON뱅킹의 통장 사본 조회 서비스를 이용해 보도록 하겠습니다. 우리WON뱅킹을 눌러 실행해 주세요.";
+const page2 = "전체 메뉴를 확인하기 위해 우측 하단의 전체를 눌러 줍니다.";
+const page3 = "계좌 조회를 눌러 줍니다.";
+const page4 = "통장사본 조회를 눌러 줍니다.";
+const page5 = "저축예금 부분을 눌러 더 자세하게 알아볼게요.";
+const page6 = "통장사본 저장을 눌러 줍니다.";
+const page7 = "통장사본 저장 페이지입니다. 한 번 눌러 넘어가 줄게요.";
+const page8 = "확인을 누르면 통장 사본이 저장됩니다. 모두 고생하셨습니다.";
 
 pageDescArray.push(
     page1,
@@ -21,7 +23,8 @@ pageDescArray.push(
     page4,
     page5,
     page6,
-    page7
+    page7,
+    page8
 );
 
 
@@ -46,33 +49,37 @@ function handleInput(pageNumber) {
 }
 
 const coordinate1 = {
-    startRateX: 0.7361111111111112, startRateY: 0.8796875, endRateX: 0.9027777777777778, endRateY: 0.9390625
+    startRateX: 0.29444444444444445, startRateY: 0.1859375, endRateX: 0.5138888888888888, endRateY: 0.05
 };
 
 const coordinate2 = {
-    startRateX: 0.3861111111111111, startRateY: 0.371875, endRateX: 0.9111111111111111, endRateY: 0.4359375
+    startRateX: 0.7361111111111112, startRateY: 0.8796875, endRateX: 0.9027777777777778, endRateY: 0.9390625
 };
 
 const coordinate3 = {
-    startRateX: 0.3888888888888889, startRateY: 0.4734375, endRateX: 0.9027777777777778, endRateY: 0.50625
+    startRateX: 0.3861111111111111, startRateY: 0.371875, endRateX: 0.9111111111111111, endRateY: 0.4359375
 };
 
 const coordinate4 = {
-    startRateX: 0.09722222222222222, startRateY: 0.3046875, endRateX: 0.9055555555555556, endRateY: 0.4703125
+    startRateX: 0.3888888888888889, startRateY: 0.4734375, endRateX: 0.9027777777777778, endRateY: 0.50625
 };
 
 const coordinate5 = {
-    startRateX: 0.325, startRateY: 0.8375, endRateX: 0.9305555555555556, endRateY: 0.946875
+    startRateX: 0.09722222222222222, startRateY: 0.3046875, endRateX: 0.9055555555555556, endRateY: 0.4703125
 };
 
 const coordinate6 = {
+    startRateX: 0.325, startRateY: 0.8375, endRateX: 0.9305555555555556, endRateY: 0.946875
+};
+
+const coordinate7 = {
     startRateX: 0,
     startRateY: 0,
     endRateX: 1,
     endRateY: 1,
 };
 
-const coordinate7 = {
+const coordinate8 = {
     startRateX: 0.1527777777777778, startRateY: 0.53125, endRateX: 0.8444444444444444, endRateY: 0.6015625
 };
 
@@ -85,5 +92,6 @@ coordinateArray.push(
     coordinate4,
     coordinate5,
     coordinate6,
-    coordinate7
+    coordinate7,
+    coordinate8
 );
--- 51/draw/change-image.js
+++ 51/draw/change-image.js
@@ -2,7 +2,7 @@ const changeButton = document.querySelector(".change-btn");
 const imageArray = [];
 
 const codeDescription = document.querySelector(".code-area-desc");
-const pageNum = 7;
+const pageNum = 8;
 const pageName = "51";
 
 for (i = 1; i <= pageNum; i++) {
