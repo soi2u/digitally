bd50eb68adc2df8576d187f846e3e55e07c8aaa5
soi2u <soi2u@daum.net>
2022-12-06 10:03:21 +0900

    221206
    수정

--- 49/draw/change-image.js
+++ 49/draw/change-image.js
@@ -6,7 +6,7 @@ const pageNum = 7;
 const pageName = "49";
 
 for (i = 1; i <= pageNum; i++) {
-    imageArray.push(`../${pageName}/image/${i}.jpg`);
+    imageArray.push(`../${pageName}/image/${i}.png`);
 }
 imageVariable = 0;
 loadBackgroundImage(imageVariable);
